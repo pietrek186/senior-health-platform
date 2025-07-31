1624d320dcc90ab1189a501c3e711a1b9dca40e2
pietrek186 <pietrek186@gmail.com>
2025-07-31 15:07:25 +0200

    Dodano funkcjonalność wyniów pomiarów ciśnienia, poprawiono wizualne aspekty

--- pressure/urls.py
+++ pressure/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
      path('', views.pressure_home, name='pressure'), 
+     path('results/', views.pressure_results, name='pressure_results'),
+     path('edit/<int:measurement_id>/', views.edit_pressure, name='edit_pressure'),
+     path('delete/<int:measurement_id>/', views.delete_pressure, name='delete_pressure'),
 ]
--- pressure/views.py
+++ pressure/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import PressureMeasurementForm
+from .models import PressureMeasurement
+from django.contrib import messages
+
 
 @login_required
 def pressure_home(request):
@@ -10,8 +13,38 @@ def pressure_home(request):
             pressure = form.save(commit=False)
             pressure.user = request.user
             pressure.save()
-            return redirect('dashboard') 
+            messages.success(request, 'Pomyślnie zapisano pomiar ciśnienia.')
+            return redirect('pressure_results')
     else:
         form = PressureMeasurementForm()
-
+        
     return render(request, 'pressure/add_pressure.html', {'form': form})
+
+
+@login_required
+def pressure_results(request):
+    measurements = PressureMeasurement.objects.filter(user=request.user).order_by('date', 'time')
+    return render(request, 'pressure/pressure_results.html', {'measurements': measurements})
+
+
+def edit_pressure(request, measurement_id):
+    measurement = get_object_or_404(PressureMeasurement, id=measurement_id, user=request.user)
+    if request.method == 'POST':
+        form = PressureMeasurementForm(request.POST, instance=measurement)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Pomyślnie zaktualizowano pomiar ciśnienia.')
+            return redirect('pressure_results')
+    else:
+        form = PressureMeasurementForm(instance=measurement)
+    return render(request, 'pressure/edit_pressure.html', {'form': form})
+
+
+@login_required
+def delete_pressure(request, measurement_id):
+    measurement = get_object_or_404(PressureMeasurement, pk=measurement_id, user=request.user)
+    if request.method == 'POST':
+        measurement.delete()
+        messages.success(request, 'Pomyślnie usunięto pomiar ciśnienia.')
+        return redirect('pressure_results')
+    return render(request, 'pressure/delete_pressure.html', {'measurement': measurement})

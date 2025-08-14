d6d245b1fe0e6f9b16b94f3f011070fc22c994e7
pietrek186 <pietrek186@gmail.com>
2025-08-14 16:33:33 +0200

    E-mail i OSM UA z .env (bez sekretów w repo)

--- zdrowie/settings.py
+++ zdrowie/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zdrowieapp2025@gmail.com'
-EMAIL_HOST_PASSWORD = 'oipm jxum xjrg tued'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
@@ -174,4 +174,5 @@ SOS_SMS_ENABLED = False ### WAŻNE Jak zmienię na True, to wyśle się normalni
 DEFAULT_GUARDIAN_EMAIL = os.environ.get("DEFAULT_GUARDIAN_EMAIL")
 
 
-OSM_USER_AGENT = "ZdrowieApp/1.0 (zdrowieapp2025@gmail.com)"
+OSM_USER_AGENT = os.environ.get("OSM_USER_AGENT", "ZdrowieApp/1.0 (zdrowieapp2025@gmail.com)")
+

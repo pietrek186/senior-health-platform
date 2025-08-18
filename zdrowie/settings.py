260950ed68d18904e085ab6adbb16c0288ea97fd
pietrek186 <pietrek186@gmail.com>
2025-08-18 21:49:57 +0200

    Przeniesiono SECRET_KEY do pliku .env

--- zdrowie/settings.py
+++ zdrowie/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uu^&fdts0mcmv8z)d6=sx=79h5^&i5*eia+7usejkqdb=dgom9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
